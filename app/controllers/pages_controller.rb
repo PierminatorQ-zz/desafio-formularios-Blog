0768f49c5a6aad159da9e2111549989c883755a3
PierminatorQ <pierrequintero@gmail.com>
2020-07-18 20:47:29 -0400

    validations included

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
 class PagesController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def dashboard
   end
 
   def create
-    Post.create(title: params[:title], image: params[:img_url], content: params[:content])
+    @post = Post.create(title: params[:title], image: params[:img_url], content: params[:content])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validate :contenido_without_spoilers
+
+
+    def contenido_without_spoilers
+        if content.include?("spoiler") == true
+            content.gsub! 'spoiler', ' '
+            
+        end
+    end
 end
