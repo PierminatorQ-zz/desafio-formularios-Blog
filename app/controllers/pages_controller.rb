cf931624ff83038c1e84616e1b3e3545e1672b5c
PierminatorQ <pierrequintero@gmail.com>
2020-07-18 21:33:20 -0400

    autenticacion added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
+  http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only:
+:dashboard
+
+
   def index
-    @posts = Post.all
+    @posts = Post.ordered_by_date
   end
 
   def dashboard
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ApplicationRecord
 
     validates :title, presence: true
+    validates :image, presence: true
+    validates :content, presence: true
     validate :contenido_without_spoilers
+    default_scope {where ("LENGTH(content) > 1")}
+    scope :ordered_by_date, -> { order(created_at: :desc) }
 
 
     def contenido_without_spoilers
